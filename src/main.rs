0f8b1309dbf72de99f946a24941be75f94fc5daa
Sergei Surovtsev <ssurovsev@gmail.com>
2022-02-23 19:33:41 +0300

    FPS with collisons

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,12 @@ enum Layer {
 
 #[derive(Component)]
 struct PlayerCamera;
+#[derive(Component)]
+struct Player;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.2, 0.8)))
         .add_plugins(DefaultPlugins)
         .add_plugin(PhysicsPlugin::default())
         .insert_resource(Gravity::from(Vec3::new(0.0, -9.81, 0.0))) // Optionally define gravity
@@ -88,26 +91,15 @@ fn spawn_game_world(
                         transform: Transform::from_translation(Vec3::new(
                             x as f32, y as f32, z as f32,
                         )),
+                        global_transform: GlobalTransform::identity(),
                         ..Default::default()
                     })
-                    .insert(GlobalTransform::default())
-                    .insert(RigidBody::Dynamic)
+                    .insert(RigidBody::Static)
                     .insert(CollisionShape::Cuboid {
                         half_extends: Vec3::new(1.0, 1.0, 1.0),
                         border_radius: None,
-                    });
-            } else {
-                if (x + y) % 10 == 0 {
-                    commands.spawn_bundle(PointLightBundle {
-                        point_light: PointLight {
-                            intensity: 500.0,
-                            shadows_enabled: false,
-                            ..Default::default()
-                        },
-                        transform: Transform::from_xyz(x as f32, y as f32, z as f32),
-                        ..Default::default()
-                    });
-                }
+                    })
+                    .insert(CollisionLayers::new(Layer::World, Layer::Player));
             }
         });
 }
@@ -138,53 +130,104 @@ pub fn spawn_player(
     commands
         .spawn_bundle((
             Transform {
-                translation: Vec3::new(33.0, 5.0, 33.0),
+                translation: Vec3::new(33.0, 1.0, 33.0),
                 rotation: Quat::from_rotation_y(-std::f32::consts::FRAC_PI_2),
                 ..Default::default()
             },
             GlobalTransform::identity(),
         ))
         .with_children(|cell| {
-            cell.spawn_scene(asset_server.load("craft_speederA.glb#Scene0"));
+            cell.spawn_bundle(PointLightBundle {
+                point_light: PointLight {
+                    intensity: 200.0,
+                    shadows_enabled: true,
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
+            // cell.spawn_scene(asset_server.load("craft_speederA.glb#Scene0"));
         })
-        .insert(CollisionShape::Cuboid {
-            half_extends: Vec3::new(1.0, 1.0, 1.0),
-            border_radius: None,
-        })
+        .insert(CollisionShape::Sphere { radius: 1.0 })
+        .insert(Player)
+        .insert(Acceleration::from_linear(Vec3::X * 0.0))
         .insert(Velocity::from_linear(Vec3::X * 0.0))
-        .insert(RigidBody::Dynamic);
+        .insert(Acceleration::from_linear(Vec3::X * 0.0))
+        .insert(RigidBody::Dynamic)
+        .insert(PhysicMaterial {
+            density: 200.0,
+            ..Default::default()
+        })
+        .insert(CollisionLayers::new(Layer::Player, Layer::World))
+        .insert(RotationConstraints::lock());
+    // .insert(RotationConstraints {
+    //     allow_x: false,
+    //     allow_y: true,
+    //     allow_z: false,
+    // });
 }
 
 fn move_player(
     keys: Res<Input<KeyCode>>,
-    mut velocities: Query<(&mut Velocity, &Transform), Without<PlayerCamera>>,
+    mut velocities: Query<(&mut Velocity, &mut Acceleration, &Transform), Without<PlayerCamera>>,
     mut cameras: Query<(&mut Transform), With<PlayerCamera>>,
+    mut events: EventReader<CollisionEvent>,
 ) {
-    for (mut velocity, transform) in velocities.iter_mut() {
+    for (mut velocity, mut acceleration, transform) in velocities.iter_mut() {
         *velocity = Velocity::from_linear(Vec3::X * 0.0);
         for key in keys.get_pressed() {
-            if *key == KeyCode::Up {
+            if *key == KeyCode::W {
+                *acceleration = Acceleration::from_linear(0.001 * transform.forward().normalize());
                 *velocity = Velocity::from_linear(10. * transform.forward().normalize());
             }
-            if *key == KeyCode::Down {
+            if *key == KeyCode::S {
+                *acceleration = Acceleration::from_linear(0.001 * -transform.forward().normalize());
                 *velocity = Velocity::from_linear(10. * -transform.forward().normalize());
             }
-            if *key == KeyCode::Left {
+            if *key == KeyCode::A {
                 *velocity = velocity.with_angular(AxisAngle::new(Vec3::Y, 0.5 * 3.14));
             }
-            if *key == KeyCode::Right {
+            if *key == KeyCode::D {
                 *velocity = velocity.with_angular(AxisAngle::new(Vec3::Y, -0.5 * 3.14));
             }
         }
+
+        events
+            .iter()
+            // We care about when the entities "start" to collide
+            // .filter(|e| e.)
+            .filter_map(|event| {
+                let (entity_1, entity_2) = event.rigid_body_entities();
+                let (layers_1, layers_2) = event.collision_layers();
+
+                println!("{:?}", layers_1);
+                println!("{:?}", layers_2);
+
+                if is_player(layers_1) && is_world(layers_2) {
+                    Some(entity_2)
+                } else if is_player(layers_2) && is_world(layers_1) {
+                    Some(entity_1)
+                } else {
+                    // This event is not the collision between an enemy and the player. We can ignore it.
+                    None
+                }
+            })
+            .for_each(|_| {
+                *velocity = Velocity::from_linear(Vec3::X * 0.0);
+            });
 
         for mut _tr in cameras.iter_mut() {
             _tr.translation.x = transform.translation.x;
             _tr.translation.z = transform.translation.z;
             _tr.translation.y = 1.0;
-
-            let fw_vec = transform.forward().normalize();
-
             _tr.rotation = Quat::from_rotation_arc(-Vec3::Z, transform.forward().normalize());
         }
     }
+}
+
+fn is_player(layers: CollisionLayers) -> bool {
+    layers.contains_group(Layer::Player) && !layers.contains_group(Layer::World)
+}
+
+fn is_world(layers: CollisionLayers) -> bool {
+    !layers.contains_group(Layer::Player) && layers.contains_group(Layer::World)
 }
